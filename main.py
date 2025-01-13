fe3877fa0e7fb632533f4d676af48cd1f4e968ae
vamg1994 <virgiliomadrid1994@gmail.com>
2025-01-13 03:35:30 -0600

    fix

--- main.py
+++ main.py
@@ -61,20 +61,7 @@ with st.sidebar:
     ### virgiliomadrid1994@gmail.com
     ### https://www.linkedin.com/in/vamadrid/
     """)
-    st.markdown("""
-    ### Enter context for better results
-    """)
-    
-"""
-    with st.container():
-        experience = st.text_input("Enter your professional experiece", placeholder="e.g., Engineer with 10 years of experience in AWS",default="")
-        achievements = st.text_input("Enter your achievements", placeholder="e.g., I won the hackathon in 2024, I built a startup named VAM, I build a AI tool to generate content",default="")
-        interests = st.text_input("Enter your interests", placeholder="e.g., I'm interested in AI, Machine Learning, Data Science, Cloud Computing, Startups, Entrepreneurship, etc.",default="general interests")
-        audience = st.text_input("Enter your audience", placeholder="e.g., general public, tech enthusiasts, entrepreneurs, founders, etc.",default="general public")
-    
-        context = f"Experience: {experience}\nAchievements: {achievements}\nInterests: {interests}\nAudience: {audience}"
-        st.session_state.generated_content['context'] = context
-"""
+
 # Title and description
 st.title("VAM Content Generator")
 st.markdown("Generate professional content in multiple formats for your topic.")
