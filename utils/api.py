911f0bf421c92e862cfe7c1cefb0aa8eb4fde245
vamg1994 <virgiliomadrid1994@gmail.com>
2025-01-14 16:48:20 -0600

    optimized ideas prompt

--- utils/api.py
+++ utils/api.py
@@ -536,12 +536,13 @@ def generate_ideas(topic: str, language: str = DEFAULT_LANGUAGE, context: str = 
                 {
                     "role": "system",
                     "content": (
+                        "Follow these guidelines:"
+                        f"Use this language:{language_instruction} "
                         f"Generate relevant ideas: Provide 3 creative approaches to the topic, considering storytelling, lists, or reflections."
                         f"Consider this context: {context}\n"
-                        f"Write everything (ideas, hook, CTA) in this language:{language_instruction} "
                         "Create a strong hook: Suggest 2-3 opening lines designed to capture immediate attention."
                         "Call to action: Include a CTA that encourages interaction or invites reflection. Suggest 2-3 closing lines"
-                        "Use recent news and events for reference when creating the ideas, hook and CTA. Provide a brief explanation of why it's relevant and links to the source with complete url"
+                        "Use recent news and events for reference when creating the ideas, hook and CTA for adding more context. Provide a brief explanation of why it's relevant and links to the source with complete url"
                         f"Use examples {example_posts} for reference when creating the ideas, hook and CTA"
                         f"Final Check: Before finishing, ensure the content has: 1)An engaging hook. 2)A flexible and coherent structure using short sentences without paragraphs. 3)A compelling CTA. 4) Source links to the news and events 5)Make sure eveything is in this language: {language_instruction}"
                     )
