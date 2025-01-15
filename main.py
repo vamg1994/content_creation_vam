ecf8509a64a383d8ef1785625d71d7ca6d3a7bf5
vamg1994 <virgiliomadrid1994@gmail.com>
2025-01-15 00:58:04 -0600

    dummy tutorial

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ with st.sidebar:
     ### Data Scientist
     """)
     st.link_button("LinkedIn 💼", "https://www.linkedin.com/in/vamadrid/")
-    st.link_button("Tutorial 📺", "")
+    st.link_button("Tutorial 📺", "https://content-generator-vam.streamlit.app/")
     st.link_button("Email 📧", "mailto:virgiliomadrid1994@gmail.com")
     st.info("""
     ### VAM Content Generator
